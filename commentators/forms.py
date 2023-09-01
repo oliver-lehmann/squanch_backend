de7d4f79902533c6eeac54827f4bb4681a757af9
Oliver Lehmann <lehmannoliver96@gmail.com>
2023-09-01 17:27:11 +0200

    style upgrade

--- commentators/forms.py
+++ commentators/forms.py
@@ -4,6 +4,12 @@ from games.models import Game
 from users.models import User
 
 class NewComentatorForm(forms.Form):
-    name = forms.ModelChoiceField(queryset=User.objects.all())
-    game = forms.ModelChoiceField(queryset=Game.objects.all())
+    name = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        widget=forms.Select(attrs={'class': 'border rounded w-full py-2 px-4'})
+    )
+    game = forms.ModelChoiceField(
+        queryset=Game.objects.all(),
+        widget=forms.Select(attrs={'class': 'border rounded w-full py-2 px-4'})
+    )
     
